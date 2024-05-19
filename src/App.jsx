16a07cd53e2e6a51e4f4945250c7e962a018672d
Iiro <iiro.paakkonen@gmail.com>
2024-05-19 21:18:10 +0300

    Some new features

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Card from "./Card";
 import Button from "./Button";
+import Student from "./Student";
 
 function App() {
   
@@ -10,6 +11,10 @@ function App() {
     <Header/>
     <Card/>
     <Button/>
+    <Student name="Susan" age={23} isStudent={true}></Student>
+    <Student name="Troy" age={25} isStudent={true}></Student>
+    <Student name="Harold" age={30} isStudent={false}></Student>
+    <Student age={19}></Student>
     <Footer/>
     </>
   );
